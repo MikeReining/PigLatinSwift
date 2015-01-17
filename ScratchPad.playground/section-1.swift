4c4ce835b56fafe774edd9144cc4d5d400daf5ef
Mike Reining <emailmike@gmail.com>
2015-01-16 21:45:30 -0800

    Added ability to split string into array of words based on blank space

--- ScratchPad.playground/section-1.swift
+++ ScratchPad.playground/section-1.swift
@@ -1,33 +1,17 @@
 // Playground - noun: a place where people can play
 
 
-var str = "Hello, playground"
-
-var myArray = ["a","b","c","d"]
-var index = 1
 
 
-func findStringIndex(array: [String], valueToFind: String) -> Int? {
-    for (index, value) in enumerate(array) {
-        if value == valueToFind {
-            return index
-        }
-    }
-    return nil
+var originalString = "First Second Third"
+
+// Turn original String into Array of Words
+
+var arrayOfWords = split(originalString) {$0 == " "}
+arrayOfWords.count
+var newWordArray = [String]()
+for word in arrayOfWords {
+    newWordArray.append(word)
 }
-
-findStringIndex(myArray, "b")!
-
-
-func buildWord(startAtIndex: Int, myArrayCount: Int, array: [String]) -> String {
-    var newWord: String = ""
-    var endOfIndex = array.count - 1
-    for index in startAtIndex...endOfIndex {
-        println("\(array[index])")
-        newWord = newWord + array[index]
-        }
-    return newWord
-}
-
-buildWord(2, myArray.count, myArray)
+newWordArray
 
